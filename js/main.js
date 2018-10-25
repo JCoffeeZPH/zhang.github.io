eb151a15f2da64401313ca3b7c65d401ba316d7a
Zhang <1787932750@qq.com>
2018-10-25 22:46:07 +0800

    Site updated: 2018-10-25 22:46:04

--- js/main.js
+++ js/main.js
@@ -1,1 +1,147 @@
-require([],function(){var i=!1,e=function(){require(["/js/mobile.js"],function(e){e.init(),i=!0})},n=!1,a=function(){require(["/js/pc.js"],function(i){i.init(),n=!0})},t={versions:function(){var i=window.navigator.userAgent;return{trident:i.indexOf("Trident")>-1,presto:i.indexOf("Presto")>-1,webKit:i.indexOf("AppleWebKit")>-1,gecko:i.indexOf("Gecko")>-1&&i.indexOf("KHTML")==-1,mobile:!!i.match(/AppleWebKit.*Mobile.*/),ios:!!i.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),android:i.indexOf("Android")>-1||i.indexOf("Linux")>-1,iPhone:i.indexOf("iPhone")>-1||i.indexOf("Mac")>-1,iPad:i.indexOf("iPad")>-1,webApp:i.indexOf("Safari")==-1,weixin:i.indexOf("MicroMessenger")==-1}}()};$(window).bind("resize",function(){if(i&&n)return void $(window).unbind("resize");var t=$(window).width();t>=700?a():e()}),t.versions.mobile===!0||$(window).width()<700?e():a(),yiliaConfig.fancybox===!0&&require(["/fancybox/jquery.fancybox.js"],function(i){var e=$(".isFancy");if(0!=e.length){for(var n=$(".article-inner img"),a=0,t=n.length;a<t;a++){var r=n.eq(a).attr("src"),o=n.eq(a).attr("alt");n.eq(a).replaceWith("<a href='"+r+"' title='"+o+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+r+"' title='"+o+"'></a>")}$(".article-inner .fancy-ctn").fancybox()}}),yiliaConfig.animate===!0&&(require(["/js/jquery.lazyload.js"],function(){$(".js-avatar").attr("src",$(".js-avatar").attr("lazy-src")),$(".js-avatar")[0].onload=function(){$(".js-avatar").addClass("show")}}),yiliaConfig.isHome===!0?require(["//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js"],function(i){function e(){$(".article").each(function(){$(this).offset().top<=$(window).scrollTop()+$(window).height()&&!$(this).hasClass("show")?($(this).removeClass("hidden").addClass("show"),$(this).addClass("is-hiddened")):$(this).hasClass("is-hiddened")||$(this).addClass("hidden")})}var n=["pulse","fadeIn","fadeInRight","flipInX","lightSpeedIn","rotateInUpLeft","slideInUp","zoomIn"],a=n.length,t=n[Math.ceil(Math.random()*a)-1];return window.requestAnimationFrame?void i({duration:0,afterReveal:function(i){$(i).addClass("animated "+t).css({opacity:1})}}).reveal(".body-wrap > article"):($(".body-wrap > article").css({opacity:1}),void(navigator.userAgent.match(/Safari/i)&&($(window).on("scroll",function(){e()}),e())))}):$(".body-wrap > article").css({opacity:1})),1==yiliaConfig.open_in_new&&($(".article a[href]").attr("target","_blank"),$(".archive-article-title").attr("target","_blank"))});
+/* 
+ * 2018.8.26 modify by lcc
+ * based on the js plugin http://www.htmleaf.com/jQuery/Menu-Navigation/20141212771.html
+ * based on other unknown sources...
+ * thanks for their open sources!
+*/
+jQuery(document).ready(function($) {
+
+	"use strict";
+
+	/* Preloader */
+	var Annie_Preloader = function() {
+		$(window).on("load", function() {
+			// fade out the loading animation
+			$("#status").fadeOut();
+
+			//fade out the white DIV that covers the website
+			$("#preloader").delay(400).fadeOut("slow");
+		});
+	};
+
+	/* Nav */
+	var Annie_Nav = function() {
+		// browser window scroll (in pixels) after which the "menu" link is shown
+		var offset = 300;
+		var navigationContainer = $('#cd-nav');
+		var mainNavigation = navigationContainer.find('#cd-main-nav ul');
+
+		//hide or show the "menu" link
+		checkMenu();
+
+		$(window).scroll(function() {
+			checkMenu();
+		});
+
+		//open or close the menu clicking on the bottom "menu" link
+		$('.cd-nav-trigger').on('click', function() {
+			$(this).toggleClass('menu-is-open');
+
+			//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
+			mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
+		});
+
+		function checkMenu() {
+			if($(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
+				navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+					mainNavigation.addClass('has-transitions');
+				});
+			} else if($(window).scrollTop() <= offset) {
+
+				//check if the menu is open when scrolling up
+				if(mainNavigation.hasClass('is-visible') && !$('html').hasClass('no-csstransitions')) {
+					//close the menu with animation
+					mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+						//wait for the menu to be closed and do the rest
+						mainNavigation.removeClass('is-visible is-hidden has-transitions');
+						navigationContainer.removeClass('is-fixed');
+						$('.cd-nav-trigger').removeClass('menu-is-open');
+					});
+
+					//check if the menu is open when scrolling up - fallback if transitions are not supported
+				} else if(mainNavigation.hasClass('is-visible') && $('html').hasClass('no-csstransitions')) {
+					mainNavigation.removeClass('is-visible has-transitions');
+					navigationContainer.removeClass('is-fixed');
+					$('.cd-nav-trigger').removeClass('menu-is-open');
+
+					//scrolling up with menu closed
+				} else {
+					navigationContainer.removeClass('is-fixed');
+					mainNavigation.removeClass('has-transitions');
+				}
+			}
+		}
+	};
+
+	/* Random bg-img for header*/
+	var Annie_Random = function() {
+		//generate a random img that pre_name 'from 0 to 110'
+		var random_bg = Math.floor(Math.random() * 109 + 1);
+
+		//var bg = 'url(/img/random/' + random_bg + '.jpg)';
+		var bg = 'url(/img/random/' + random_bg + '.jpg)';
+
+		$("#header-bg-2").css("background-image", bg);
+	};
+
+	/* ToTop */
+	var Annie_ToTop = function() {
+		var upperLimit = 500;
+
+		// Our scroll link element
+		var scrollElem = $('#totop');
+
+		// Scroll to top speed
+		var scrollSpeed = 500;
+
+		scrollElem.hide();
+
+		$(window).scroll(function() {
+			var scrollTop = $(document).scrollTop();
+
+			if(scrollTop > upperLimit) {
+				$(scrollElem).stop().fadeTo(300, 1);
+			} else {
+				$(scrollElem).stop().fadeTo(300, 0);
+			}
+		});
+
+		$(scrollElem).click(function() {
+			$('html, body').animate({
+				scrollTop: 0
+			}, scrollSpeed);
+			return false;
+		});
+	};
+
+	/* Show Comment */
+	var Annie_Comment = function() {
+		function Show_Hidden(obj) {
+			var obj = $('#annie-comment-container');
+		}
+
+		var obutton = document.getElementById("annie-comment-button");
+		var odiv = document.getElementById("annie-comment-container");
+		//var obutton = $('#annie-comment-button');
+		//var odiv = $('#annie-comment-container');	
+		if('obutton') {
+			obutton.onclick = function() {
+				Show_Hidden(odiv);
+				$("#annie-comment-button").css("display", 'none');
+				return false;
+			}
+		}
+	};
+
+	/* other js function */
+	/* ... */
+
+	/* Initialize */
+	(function Annie_Init() {
+		Annie_Preloader();
+		Annie_Nav();
+		//Annie_Random();
+		Annie_ToTop();
+		//Annie_Comment();
+	})();
+});
